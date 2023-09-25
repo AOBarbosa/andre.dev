d1c1665afbf873a89b669f05eb03313d4f58b2dc
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2023-09-25 14:02:06 -0300

    Navigation Menu configured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,31 @@
 'use client'
 
 import { Code2 } from 'lucide-react'
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-  navigationMenuTriggerStyle,
-} from '@/components/ui/navigation-menu'
 import { TypingWritter } from '@/components/TypingWritter'
+import { NavMenu } from '@/components/NavMenu'
 
 export default function Home() {
   return (
     <div className="w-full h-full px-40 flex-col items-center justify-center">
-      <nav className="bg-red-400 w-full flex flex-row justify-between">
-        <Code2 />
+      <nav className="w-full h-full flex flex-row justify-between items-center">
+        <Code2 className="w-12 h-12" />
 
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>About</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul>
-                  <li>Me</li>
-                  <li>Projects</li>
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+        <NavMenu />
       </nav>
 
-      <div className="bg-[#080713] flex justify-center items-center h-screen w-full text-2xl">
-        <h1 className="font-bold">
-          <TypingWritter content="Hello, i'm" showCursor />
+      <main>
+        <div className="h-full w-full text-3xl">
+          <h1 className="font-bold">
+            <TypingWritter content="Hello, i'm" showCursor />
 
-          <span className="text-3xl text-purple-600">
-            <TypingWritter content="André Barbosa" delay={1300} showCursor />
-          </span>
+            <span className="text-5xl text-purple-600">
+              <TypingWritter content="André Barbosa" delay={1300} showCursor />
+            </span>
 
-          <TypingWritter content="a Front-end Developer" delay={2800} />
-        </h1>
-      </div>
+            <TypingWritter content="a Front-end Developer" delay={2800} />
+          </h1>
+        </div>
+      </main>
     </div>
   )
 }
---
+++ src/components/NavMenu.tsx
@@ -0,0 +1,119 @@
+'use client'
+
+import * as React from 'react'
+import Link from 'next/link'
+
+import { cn } from '@/lib/utils'
+// import { Icons } from '@/components/icons'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu'
+
+const about: { title: string; href: string; description: string }[] = [
+  {
+    title: 'Me',
+    href: '/',
+    description: 'Learn a bit more about me and what I do.',
+  },
+  {
+    title: 'Projects',
+    href: '/',
+    description:
+      'All the projects I have been developing throughout my journey.',
+  },
+]
+
+const setup: { title: string; href: string; description: string }[] = [
+  {
+    title: 'Setup',
+    href: '/',
+    description: 'All the equipment I use in my daily life.',
+  },
+  {
+    title: 'Stack',
+    href: '/',
+    description: 'The technologies I use as a developer.',
+  },
+]
+
+export function NavMenu() {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>About</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="w-96 gap-3 p-4">
+              {about.map((about) => (
+                <ListItem
+                  key={about.title}
+                  title={about.title}
+                  href={about.href}
+                >
+                  {about.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Setup</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="w-96 gap-3 p-4">
+              {setup.map((setup) => (
+                <ListItem
+                  key={setup.title}
+                  title={setup.title}
+                  href={setup.href}
+                >
+                  {setup.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Contact
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  )
+}
+
+const ListItem = React.forwardRef<
+  React.ElementRef<'a'>,
+  React.ComponentPropsWithoutRef<'a'>
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+            className,
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = 'ListItem'
