d4af14599d795844bf3c9feb22a1bc0ad0937903
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2024-01-16 17:54:12 -0300

    Stack page is now responsive

--- src/app/setup/stack/page.tsx
+++ src/app/setup/stack/page.tsx
@@ -12,6 +12,7 @@ import reactTestLibIMG from '@/assets/react-test-lib.png'
 import jestIMG from '@/assets/jest-plain.svg'
 import cypressIMG from '@/assets/cypress-logo.png'
 import nodeIMG from '@/assets/nodejs-original.svg'
+import Link from 'next/link'
 
 export default function Stack() {
   return (
@@ -19,80 +20,152 @@ export default function Stack() {
       <div className="flex flex-col items-center gap-4">
         <h1 className="text-4xl">Stack</h1>
         <p className="text-lg text-muted-foreground">
-          Seguem as tecnologias e serviços que utilizo no meu dia a dia.
+          Here are the technologies and services I use in my daily life.
         </p>
       </div>
 
-      <main className="grid grid-cols-4 gap-4 mt-6">
+      <main className="grid grid-cols-2 xs:grid-cols-3 mm:grid-cols-4 gap-4 mt-6">
         <Card className="w-48 h-48 flex items-center justify-center">
           <CardContent>
-            <Image className="mt-6" src={htmlIMG} alt="" />
+            <Link href="https://developer.mozilla.org/en-US/docs/Web/HTML">
+              <Image
+                className="mt-6 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+                src={htmlIMG}
+                alt=""
+              />
+            </Link>
           </CardContent>
         </Card>
 
         <Card className="w-48 h-48 flex items-center justify-center">
           <CardContent>
-            <Image className="mt-6" src={cssIMG} alt="" />
+            <Link href="https://developer.mozilla.org/pt-BR/docs/Web/CSS">
+              <Image
+                className="mt-6 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+                src={cssIMG}
+                alt=""
+              />
+            </Link>
           </CardContent>
         </Card>
 
         <Card className="w-48 h-48 flex items-center justify-center">
           <CardContent>
-            <Image className="mt-6" src={jsIMG} alt="" />
+            <Link href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript">
+              <Image
+                className="mt-6 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+                src={jsIMG}
+                alt=""
+              />
+            </Link>
           </CardContent>
         </Card>
 
         <Card className="w-48 h-48 flex items-center justify-center">
           <CardContent>
-            <Image className="mt-6" src={tsIMG} alt="" />
+            <Link href="https://www.typescriptlang.org/">
+              <Image
+                className="mt-6 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+                src={tsIMG}
+                alt=""
+              />
+            </Link>
           </CardContent>
         </Card>
 
         <Card className="w-48 h-48 flex items-center justify-center">
           <CardContent>
-            <Image className="mt-6" src={swiftIMG} alt="" />
+            <Link href="https://www.swift.org/">
+              <Image
+                className="mt-6 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+                src={swiftIMG}
+                alt=""
+              />
+            </Link>
           </CardContent>
         </Card>
 
         <Card className="w-48 h-48 flex items-center justify-center">
           <CardContent>
-            <Image className="mt-6" src={reactIMG} alt="" />
+            <Link href="https://react.dev/">
+              <Image
+                className="mt-6 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+                src={reactIMG}
+                alt=""
+              />
+            </Link>
           </CardContent>
         </Card>
 
         <Card className="w-48 h-48 flex items-center justify-center">
           <CardContent>
-            <Image className="mt-6" src={nodeIMG} alt="" />
+            <Link href="https://nodejs.org/en">
+              <Image
+                className="mt-6 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+                src={nodeIMG}
+                alt=""
+              />
+            </Link>
           </CardContent>
         </Card>
 
         <Card className="w-48 h-48 flex items-center justify-center">
           <CardContent>
-            <Image className="mt-6" src={nextIMG} alt="" />
+            <Link href="https://nextjs.org/">
+              <Image
+                className="mt-6 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+                src={nextIMG}
+                alt=""
+              />
+            </Link>
           </CardContent>
         </Card>
 
         <Card className="w-48 h-48 flex items-center justify-center">
           <CardContent>
-            <Image className="mt-6" src={tailwindIMG} alt="" />
+            <Link href="https://tailwindcss.com/">
+              <Image
+                className="mt-6 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+                src={tailwindIMG}
+                alt=""
+              />
+            </Link>
           </CardContent>
         </Card>
 
         <Card className="w-48 h-48 flex items-center justify-center">
           <CardContent>
-            <Image className="mt-6" src={reactTestLibIMG} alt="" />
+            <Link href="https://testing-library.com/">
+              <Image
+                className="mt-6 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+                src={reactTestLibIMG}
+                alt=""
+              />
+            </Link>
           </CardContent>
         </Card>
 
         <Card className="w-48 h-48 flex items-center justify-center">
           <CardContent>
-            <Image className="mt-6" src={jestIMG} alt="" />
+            <Link href="https://jestjs.io/">
+              <Image
+                className="mt-6 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+                src={jestIMG}
+                alt=""
+              />
+            </Link>
           </CardContent>
         </Card>
 
         <Card className="w-48 h-48 flex items-center justify-center">
           <CardContent>
-            <Image className="mt-6" src={cypressIMG} alt="" />
+            <Link href="https://www.cypress.io/">
+              <Image
+                className="mt-6 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"
+                src={cypressIMG}
+                alt=""
+              />
+            </Link>
           </CardContent>
         </Card>
       </main>
